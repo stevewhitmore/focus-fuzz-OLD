deca71219ab06ee1803ab1e0925d4853f6544c45
stevewhitmore <s25w89@gmail.com>
2021-10-02 15:35:02 -0500

    redoing the whole idea

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SoundService } from './services/sound.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  selector: 'ff-root',
+  template: `
+    <h1>Focus Fuzz</h1>
+    <section>
+      <ff-sound-wrapper></ff-sound-wrapper>
+    </section>
+  `,
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'focus-fuzz';
+export class AppComponent implements OnInit {
+  
+  constructor(private soundService: SoundService) { }
+
+  ngOnInit() {
+    
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { SoundWrapperComponent } from './sound-wrapper/sound-wrapper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SoundWrapperComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
